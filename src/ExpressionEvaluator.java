ef67f28699a20057c1ef304ef963ab54b54185a3
Nanaanaaa <3516892226@qq.com>
2023-12-30 13:44:54 +0800

    233

--- src/ExpressionEvaluator.java
+++ src/ExpressionEvaluator.java
@@ -1,34 +1,64 @@
 import java.util.*;
+
+// 双目运算符接口
 interface BinaryOperator {
-    double apply(double leftOperand, double rightOperand);
-    int getPrecedence();
-    ExpressionEvaluator.Associativity getAssociativity();
+    double apply(double leftOperand, double rightOperand); // 运算的接口
+
+    int getPrecedence(); // 优先级的接口
+
+    ExpressionEvaluator.Associativity getAssociativity(); // 运算结合性的接口
 }
 
 public class ExpressionEvaluator {
 
-    // 运算符优先级
+    // 这是固有运算符加减乘除和它们的优先级
     private static final Map<String, Integer> OPERATOR_PRECEDENCE = new HashMap<>();
     static {
-        OPERATOR_PRECEDENCE.put("+", 1);
-        OPERATOR_PRECEDENCE.put("-", 1);
-        OPERATOR_PRECEDENCE.put("*", 2);
-        OPERATOR_PRECEDENCE.put("/", 2);
+        OPERATOR_PRECEDENCE.put("+", 5);
+        OPERATOR_PRECEDENCE.put("-", 5);
+        OPERATOR_PRECEDENCE.put("*", 4);
+        OPERATOR_PRECEDENCE.put("/", 4);
     }
 
-    // 自定义的运算符
+    // 这是自定义的运算符
     private static final Map<String, BinaryOperator> CUSTOM_OPERATORS = new HashMap<>();
     static {
-        // 自定义运算符：求幂
+        final double eps = 1e-6;
+        // 自定义运算符: 取模
+        CUSTOM_OPERATORS.put("%", new BinaryOperator() {
+            @Override
+            public double apply(double leftOperand, double rightOperand) {
+                return leftOperand % rightOperand;
+            }
+
+            @Override
+            public int getPrecedence() {
+                return 4;
+            }
+
+            @Override
+            public Associativity getAssociativity() {
+                return Associativity.RIGHT_TO_LEFT;
+            }
+        });
+        // 自定义运算符: 异或
         CUSTOM_OPERATORS.put("^", new BinaryOperator() {
             @Override
             public double apply(double leftOperand, double rightOperand) {
-                return Math.pow(leftOperand, rightOperand);
+                long t1 = Math.round(leftOperand), t2 = Math.round(rightOperand);
+                double delta1 = Math.abs(t1 - leftOperand), delta2 = Math.abs(t2 - rightOperand);
+
+                if (delta1 >= eps || delta2 >= eps) {
+                    throw new UnsupportedOperationException("Unimplemented method 'apply'");
+                }
+                return t1 ^ t2;
             }
+
             @Override
             public int getPrecedence() {
-                return 3;
+                return 10;
             }
+
             @Override
             public Associativity getAssociativity() {
                 return Associativity.RIGHT_TO_LEFT;
@@ -42,8 +72,6 @@ public class ExpressionEvaluator {
         RIGHT_TO_LEFT
     }
 
-    // 双目运算符接口
-
     // 判断字符是否为数字
     private static boolean isDigit(char ch) {
         return Character.isDigit(ch);
@@ -51,7 +79,9 @@ public class ExpressionEvaluator {
 
     // 判断字符是否为运算符
     private static boolean isOperator(char ch) {
-        return OPERATOR_PRECEDENCE.containsKey(Character.toString(ch)) || CUSTOM_OPERATORS.containsKey(Character.toString(ch));
+        boolean fred1 = OPERATOR_PRECEDENCE.containsKey(Character.toString(ch));
+        boolean fred2 = CUSTOM_OPERATORS.containsKey(Character.toString(ch));
+        return fred1 || fred2;
     }
 
     // 判断字符是否为左括号
@@ -68,35 +98,45 @@ public class ExpressionEvaluator {
     public static List<String> toPostfix(String infixExpression) {
         List<String> postfixTokens = new ArrayList<>(); // 用于存储后缀表达式的token
         Deque<Character> operatorStack = new ArrayDeque<>(); // 用于存储运算符的栈
-
+        final int N = infixExpression.length();
         // 处理每个字符
-        for (int i = 0; i < infixExpression.length(); i++) {
+        for (int i = 0; i < N; i++) {
             char ch = infixExpression.charAt(i);
             if (ch == ' ') {
                 continue;
             }
             if (isDigit(ch)) { // 如果是数字，读取整个数字并添加到后缀表达式中
                 StringBuilder builder = new StringBuilder();
-                builder.append(ch);
-                while (i + 1 < infixExpression.length() && isDigit(infixExpression.charAt(i+1))) {
-                    builder.append(infixExpression.charAt(++i));
+                builder.append(infixExpression.charAt(i));
+                int j = i;
+                while (j + 1 < N) {
+                    final char c = infixExpression.charAt(j + 1);
+                    if (isDigit(c) || c == '.') {
+                        builder.append(c);
+                        j++;
+                    } else {
+                        break;
+                    }
                 }
+                i = j;
                 postfixTokens.add(builder.toString());
             } else if (isOperator(ch)) { // 如果是运算符
                 String operator = Character.toString(ch);
                 BinaryOperator binaryOperator = CUSTOM_OPERATORS.get(operator);
-                int precedence = (binaryOperator != null) ? binaryOperator.getPrecedence() : OPERATOR_PRECEDENCE.get(operator);
-                Associativity associativity = (binaryOperator != null) ? binaryOperator.getAssociativity() : Associativity.LEFT_TO_RIGHT;
+                int precedence = (binaryOperator != null) ? binaryOperator.getPrecedence()
+                        : OPERATOR_PRECEDENCE.get(operator);
+                Associativity associativity = (binaryOperator != null) ? binaryOperator.getAssociativity()
+                        : Associativity.LEFT_TO_RIGHT;
 
                 // 将栈中优先级高于或等于当前运算符的运算符弹出并添加到后缀表达式中
                 while (!operatorStack.isEmpty() && isOperator(operatorStack.peek())) {
                     String topOperator = Character.toString(operatorStack.peek());
                     BinaryOperator topBinaryOperator = CUSTOM_OPERATORS.get(topOperator);
-                    int topPrecedence = (topBinaryOperator != null) ? topBinaryOperator.getPrecedence() : OPERATOR_PRECEDENCE.get(topOperator);
-                    Associativity topAssociativity = (topBinaryOperator != null) ? topBinaryOperator.getAssociativity() : Associativity.LEFT_TO_RIGHT;
+                    int topPrecedence = (topBinaryOperator != null) ? topBinaryOperator.getPrecedence()
+                            : OPERATOR_PRECEDENCE.get(topOperator);
 
-                    if ((associativity == Associativity.LEFT_TO_RIGHT && precedence <= topPrecedence) ||
-                            (associativity == Associativity.RIGHT_TO_LEFT && precedence < topPrecedence)) {
+                    if ((associativity == Associativity.LEFT_TO_RIGHT && precedence >= topPrecedence) ||
+                            (associativity == Associativity.RIGHT_TO_LEFT && precedence > topPrecedence)) {
                         postfixTokens.add(Character.toString(operatorStack.pop()));
                     } else {
                         break;
